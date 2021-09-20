9420aa3371c26e9d2a2334dee4ccd88760ecb8ad
brenoaugustoalves <brenoaugustoalves@gmail.com>
2021-09-20 13:44:19 -0300

    projeto gubee

---
+++ src/java/Gubee/Produto.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gubee;
+
+/**
+ *
+ * @author Breno
+ */
+public class Produto {
+    
+    String nomeproduto;
+    String descricao; 
+    String mercadoalvo;
+    String tecnologianoproduto;
+
+    public String getNomeproduto() {
+        return nomeproduto;
+    }
+
+    public void setNomeproduto(String nomeproduto) {
+        this.nomeproduto = nomeproduto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getMercadoalvo() {
+        return mercadoalvo;
+    }
+
+    public void setMercadoalvo(String mercadoalvo) {
+        this.mercadoalvo = mercadoalvo;
+    }
+
+    public String getTecnologianoproduto() {
+        return tecnologianoproduto;
+    }
+
+    public void setTecnologianoproduto(String tecnologianoproduto) {
+        this.tecnologianoproduto = tecnologianoproduto;
+    }
+    
+}
---
+++ src/java/Gubee/main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gubee;
+
+import javax.json.JsonObject;
+import javax.json.stream.JsonParser;
+
+
+
+/**
+ *
+ * @author Breno
+ */
+public class main {
+    
+    
+    
+    public static void main (String[]args) {
+    
+    
+    JsonObject jsonObject;
+    
+    JsonParser parser = new JsonParser();
+    
+    Produto produto = new Produto();
+    
+    
+    
+    }
+    
+    
+    
+}
